1bb16d4f5d55db069a99af593b5964a9872ed460
GeekLad <geeklad@geeklad.com>
2023-03-10 21:58:22 +0000

    Added back single-segment fetch script

---
+++ load-segment.py
@@ -0,0 +1,22 @@
+# Sample usage:
+#     python3 load-segment.py -s 967082162553397800_5102_900_5122_900 -b waymo-processed-images -d validation
+
+# Imports
+import autonomoeye
+import google.auth
+import tensorflow as tf
+import argparse
+import autonomoeye
+
+# Setup credentials
+credentials, _ = google.auth.default()
+
+# Parse the args
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', dest='bucket')
+parser.add_argument('-s', dest='segment')
+parser.add_argument('-d', dest='datatype')
+args=parser.parse_args()
+
+# Fetch & process
+autonomoeye.fetch_segment(args.bucket, args.datatype, args.segment)
